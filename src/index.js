c20022dcbde8578e0c5e74cb6c4d30e9cdea3e5c
syedmhdm <116684611+syedmhdm@users.noreply.github.com>
2023-09-12 01:52:51 +0530

    onSetRating is configured

--- src/index.js
+++ src/index.js
@@ -8,6 +8,17 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      // onSetRating={}
+    />
+    <StarRating
+      maxRating={7}
+      size={30}
+      color={"red"}
+      className='test'
+      defaultRating={3}
+    />
   </React.StrictMode>
 );
