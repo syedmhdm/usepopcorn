e5e417bcd958cd2a91da9f530bfc88e81fad8a8d
syedmhdm <116684611+syedmhdm@users.noreply.github.com>
2023-10-02 14:11:21 +0530

    aborting request in cleanup function

--- src/App.js
+++ src/App.js
@@ -46,12 +46,15 @@ export default function App() {
   }
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&S=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&S=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok)
@@ -60,9 +63,13 @@ export default function App() {
           const data = await res.json();
           if (data.Response === "False") throw new Error("Movies not found!");
           setMovies(data.Search);
+          setError("");
         } catch (err) {
           console.error(err.message);
-          setError(err.message);
+
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -73,6 +80,10 @@ export default function App() {
         return;
       }
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
